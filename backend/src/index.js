f76dbf27e818f9dbd8859531d52089e00d430102
MANPREETS47 <manpreetsinghms1080@gmail.com>
2025-07-18 22:49:44 +0530

    Updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,6 +24,14 @@ app.use(cors({
     credentials: true
 }))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 
 
