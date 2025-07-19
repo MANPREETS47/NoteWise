ab07a8130849232b2aba3a9c4cb4d5d72ea49a4b
MANPREETS47 <manpreetsinghms1080@gmail.com>
2025-07-19 14:49:10 +0530

    Updated token

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,10 +5,10 @@ export const generateToken = (userId, res) => {
         expiresIn: '7d'
     });
 
-    res.cookie('jwt', token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-        sameSite: 'strict', // Prevent CSRF attacks
-    })
+    // res.cookie('jwt', token, {
+    //     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    //     httpOnly: true,
+    //     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    //     sameSite: 'strict', // Prevent CSRF attacks
+    // })
 }
